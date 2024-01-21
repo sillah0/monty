dfcb5d0b060657682bbabfabd62d32298573af4a
sillah0 <siladennis624@gmail.com>
2024-01-21 23:41:42 +0300

    new

--- opcode_1_funcs.c
+++ opcode_1_funcs.c
@@ -19,3 +19,25 @@ void add_function(stack_t **stack, unsigned int line_number)
 	*stack = temp->next;
 	free(temp);
 }
+
+
+/**
+ * sub_function - diffrencebtwn 2 elements
+ * @satck: a pointer
+ * @line_number _ line number
+*/
+void sub_function(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+
+	if (!stack || !*stack || !(*stack)->next)
+	{
+		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	temp = *stack;
+	temp->next->n -= temp->n;
+	temp->next->prev = NULL;
+	*stack = temp->next;
+	free(temp);
+}
